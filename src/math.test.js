29906943273188145b18ddca7328ed256eece2e4
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-06 11:28:56 +0530

    first test with AAA pattern

---
+++ src/math.test.js
@@ -0,0 +1,17 @@
+import { expect, it } from "vitest";
+import { add } from "./math";
+
+it("summarize all number values in an array", () => {
+  // Arrange
+  const numbers = [1, 2];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
