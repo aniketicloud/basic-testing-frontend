549630ff890337de64fdd9e1f2a5e61a9757755b
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-06 11:41:06 +0530

    defining behaviours & fixing errors in the code

--- src/math.test.js
+++ src/math.test.js
@@ -15,3 +15,25 @@ it("summarize all number values in an array", () => {
   );
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one number is provided", () => {
+  // Arrange
+  const numbers = [1, "two"];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
