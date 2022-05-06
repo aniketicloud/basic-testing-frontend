f9900baf5743c6a9b4384982b7db06ad970f262b
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2022-05-06 12:27:59 +0530

    testing for errors

--- src/math.test.js
+++ src/math.test.js
@@ -47,6 +47,8 @@ it("should yield 0 if an empty array is provided", () => {
 });
 
 it.only("should throw an error if no value is passed into the function", () => {
-  const result = add();
-  // expect(result);
+  const resultFn = () => {
+    add();
+  };
+  expect(resultFn).toThrow();
 });
